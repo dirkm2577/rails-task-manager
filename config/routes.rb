69f80e23c33b254f6fb76bc8358fb6aab5fd25fe
Dirk Mischke <dee2577@gmail.com>
2022-10-15 13:09:23 +0200

    added all functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tasks
 end
+
+# Read all tasks
+  # get 'tasks', to: 'tasks#index'
+  # Add new task
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # Read one task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # Update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
